05a6a53b24ffb778c8cfb69a9dfa65f96b7612f1
garry5296 <garry5296@gmail.com>
2024-10-01 02:50:09 +0530

    partial assignment12.c

---
+++ assignment12.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+void printN(int);
+void printRevN(int);
+void printOdd(int);
+void printRevOdd(int);
+void printEven(int);
+void printRevEven(int);
+void printSq(int);
+int main()
+{
+    // printf("\n\n\e[1mQuestion1. Write a recursive function to print first N natural numbers.\e[m");
+    // int a;
+    // printf("\nEnter a number:");
+    // scanf("%d",&a);
+    // printN(a);
+
+    // printf("\n\n\e[1mQuestion2. Write a recursive function to print first N natural numbers in reverse order.\e[m");
+    // int b;
+    // printf("\nEnter a number:");
+    // scanf("%d",&b);
+    // printRevN(b);
+
+    // printf("\n\n\e[1mQuestion3. Write a recursive function to print first N odd natural numbers.\e[m");
+    // int e;
+    // printf("\nEnter a number:");
+    // scanf("%d",&e);
+    // printOdd(e);
+
+
+    // printf("\n\n\e[1mQuestion4. Write a recursive function to print first N odd natural numbers in reverse order.\e[m");
+    // int g;
+    // printf("\nEnter a number:");
+    // scanf("%d",&g);
+    // printRevOdd(g);
+
+    // printf("\n\n\e[1mQuestion5. Write a recursive function to print first N even natural numbers.\e[m");
+    // int c;
+    // printf("\nEnter a number:");
+    // scanf("%d",&c);
+    // printEven(c);
+    
+    // printf("\n\n\e[1mQuestion6. Write a recursive function to print first N even natural numbers in reverse order.\e[m");
+    // int d;
+    // printf("\nEnter a number:");
+    // scanf("%d",&d);
+    // printRevEven(d);
+    
+    printf("\n\n\e[1mQuestion7. Write a recursive function to print squares of first N natural numbers.\e[m");
+    int g;
+    printf("\nEnter a number:");
+    scanf("%d",&g);
+    printSq(g);
+    
+    // printf("\n\n\e[1mQuestion8. Write a recursive function to print binary of a given decimal number.\e[m");
+    // printf("\n\n\e[1mQuestion9. Write a recursive function to print octal of a given decimal number.\e[m");
+    // printf("\n\n\e[1mQuestion10. Write a recursive function to print reverse of a given number.\e[m");
+
+    return 0;
+}
+//functions
+void printN(int n)
+{
+    if(n>0)
+    {
+        printN(n-1);
+        printf("%d ",n);
+    }
+}
+void printRevN(int n)
+{
+    if(n>0)
+    {
+        printf("%d ",n);
+        printRevN(n-1);
+    }
+}
+void printEven(int n)
+{
+    if(n>0)
+    {
+        printEven(n-1);
+        printf("%d ",2*n);
+    }
+}
+void printRevEven(int n)
+{
+    if(n>0)
+    {
+        printf("%d ",2*n);
+        printRevEven(n-1);
+    }
+}
+void printOdd(int n)
+{
+    if(n>0)
+    {
+        printOdd(n-1);
+        printf("%d ",2*n-1);
+    }
+}
+void printRevOdd(int n)
+{
+    if(n>0)
+    {
+        printf("%d ",2*n-1);
+        printRevOdd(n-1);
+    }
+}
+void printSq(int n)
+{
+    if(n>0)
+    {
+        printSq(n-1);
+        printf("%d ",n*n);
+    }
+}
