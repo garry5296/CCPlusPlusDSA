135d84400298a21b2c13cbe3424ed0d1a8621db7
garry5296 <garry5296@gmail.com>
2025-01-19 02:36:44 +0530

    tictactoegame.cpp

--- assignment25.cpp
+++ assignment25.cpp
@@ -1,242 +1,245 @@
 #include<iostream>
 using namespace std;
+namespace assign25
+{
+	class Complex{
+		private:
+			int real,imag;
+		public:
+			void getValue()
+			{
+				cout<<endl<<"Enter real and imag part of complex number: ";
+				cin>>real>>imag;
+			}
+			void disPlay()
+			{
+				cout<<real<<"+"<<imag<<"i"<<endl;
+			}
+	};
+	class Time{
+		private:
+			int hr,min,sec;
+		public:
+			void getValue()
+			{
+				input:
+				cout<<endl<<"Enter hours, minutes and seconds: ";
+				cin>>hr>>min>>sec;
+				if((hr<0 || hr>23) || (min<0 || min>60) || (sec<0 || sec>60))
+				{
+					cout<<endl<<"Error: Invalid Time!!";
+					goto input;
+				}
+			}
+			void disPlay()
+			{
+				cout<<hr<<" hr "<<min<<" min "<<sec<<" sec "<<endl;
+			}
+	};
+	class Factorial{
+			int num;
+		public:
+			void getVal()
+			{
+				cout<<endl<<"Enter a number to calculate factorial: ";
+				cin>>num;
+			}
+			int fact()
+			{
+				int prod=1;
+				while(num>=1)
+				{
+					prod*=num;
+					num--;
+				}
+				return prod;
+			}
+	};
+	class LargestNumber{
+		private:
+			int num1,num2,num3;
+		public:
+			void getVal()
+			{
+				cout<<endl<<"Enter three numbers: ";
+				cin>>num1>>num2>>num3;
+			}
+			int largestNum()
+			{
+				return (num1>num2)?((num1>num3)?num1:num3):((num2>num3)?num2:num3);
+			}
+	};
+	class ReverseNumber{
+		private:
+			long int num;
+			void numRev(int n)
+			{
+				if(n%10)
+				{
+					cout<<n%10;
+					numRev(n/10);
+				}
+			}
+		public:
+			void getVal()
+			{
+				cout<<endl<<"Enter a number to find it's reverse: ";
+				cin>>num;
+				if(num<0)
+					num=-num;
+			}
+			void numRevMain()
+			{
+				numRev(num);
+			}
 
-class Complex{
-	private:
-		int real,imag;
-	public:
-		void getValue()
-		{
-			cout<<endl<<"Enter real and imag part of complex number: ";
-			cin>>real>>imag;
-		}
-		void disPlay()
-		{
-			cout<<real<<"+"<<imag<<"i"<<endl;
-		}
-};
-class Time{
-	private:
-		int hr,min,sec;
-	public:
-		void getValue()
-		{
-			input:
-			cout<<endl<<"Enter hours, minutes and seconds: ";
-			cin>>hr>>min>>sec;
-			if((hr<0 || hr>23) || (min<0 || min>60) || (sec<0 || sec>60))
+	};
+	class Square{
+		private:
+			int num;
+			static int n;
+		public:
+			void getVal()
 			{
-				cout<<endl<<"Error: Invalid Time!!";
-				goto input;
+				cout<<endl<<"Enter a number: ";
+				cin>>num;
 			}
-		}
-		void disPlay()
-		{
-			cout<<hr<<" hr "<<min<<" min "<<sec<<" sec "<<endl;
-		}
-};
-class Factorial{
-		int num;
-	public:
-		void getVal()
-		{
-			cout<<endl<<"Enter a number to calculate factorial: ";
-			cin>>num;
-		}
-		int fact()
-		{
-			int prod=1;
-			while(num>=1)
+			float numSq()
 			{
-				prod*=num;
-				num--;
+				return num*num;
 			}
-			return prod;
-		}
-};
-class LargestNumber{
-	private:
-		int num1,num2,num3;
-	public:
-		void getVal()
-		{
-			cout<<endl<<"Enter three numbers: ";
-			cin>>num1>>num2>>num3;
-		}
-		int largestNum()
-		{
-			return (num1>num2)?((num1>num3)?num1:num3):((num2>num3)?num2:num3);
-		}
-};
-class ReverseNumber{
-	private:
-		long int num;
-		void numRev(int n)
-		{
-			if(n%10)
+	};
+	class Greatest{
+		private:
+			int num1,num2,num3;
+		public:
+			void getVal()
 			{
-				cout<<n%10;
-				numRev(n/10);
+				cout<<endl<<"Enter three numbers: ";
+				cin>>num1>>num2>>num3;
 			}
-		}
-	public:
-		void getVal()
-		{
-			cout<<endl<<"Enter a number to find it's reverse: ";
-			cin>>num;
-			if(num<0)
-				num=-num;
-		}
-		void numRevMain()
-		{
-			numRev(num);
-		}
+			int greatestNum()
+			{
+				return (num1>num2)?((num1>num3)?num1:num3):((num2>num3)?num2:num3);
+			}
+	};
+	class Rectangle{
+		private:
+			int l,b;
+		public:
+			void getVal()
+			{
+				cout<<endl<<"Enter length and breadth of a rectangle: ";
+				cin>>l>>b;
+				if(l<0)
+					l=-l;
+				if(b<0)
+					b=-b;
+			}
+			float recArea()
+			{
+				return l*b;
+			}
+	};
+	class Circle{
+		private:
+			int r;
+		public:
+			void getVal()
+			{
+				cout<<endl<<"Enter radius of the circle: ";
+				cin>>r;
+				if(r<0)
+					r=-r;
+			}
+			float circleArea()
+			{
+				return 3.14*r*r;
+			}
+	};
+	class Area{
+		private:
+			int r,len,bre,side;
+		public:
+			void getDataCircle(int a)
+			{
+				r=a;
+			}
+			void getDataRectangle(int a,int b)
+			{
+				len=a;
+				bre=b;
+			}
+			void getDataSquare(int a)
+			{
+				side=a;
+			}
+			void circleArea()
+			{
+				cout<<endl<<"Area of circle is: "<<r*r*3.14;
+			}
+			void rectangleArea()
+			{
+				cout<<endl<<"Area of rectangle is: "<<len*bre;
+			}
+			void squareArea()
+			{
+				cout<<endl<<"Area of square is: "<<side*side;
+			}
+	};
+}
 
-};
-class Square{
-	private:
-		int num;
-		static int n;
-	public:
-		void getVal()
-		{
-			cout<<endl<<"Enter a number: ";
-			cin>>num;
-		}
-		float numSq()
-		{
-			return num*num;
-		}
-};
-class Greatest{
-	private:
-		int num1,num2,num3;
-	public:
-		void getVal()
-		{
-			cout<<endl<<"Enter three numbers: ";
-			cin>>num1>>num2>>num3;
-		}
-		int greatestNum()
-		{
-			return (num1>num2)?((num1>num3)?num1:num3):((num2>num3)?num2:num3);
-		}
-};
-class Rectangle{
-	private:
-		int l,b;
-	public:
-		void getVal()
-		{
-			cout<<endl<<"Enter length and breadth of a rectangle: ";
-			cin>>l>>b;
-			if(l<0)
-				l=-l;
-			if(b<0)
-				b=-b;
-		}
-		float recArea()
-		{
-			return l*b;
-		}
-};
-class Circle{
-	private:
-		int r;
-	public:
-		void getVal()
-		{
-			cout<<endl<<"Enter radius of the circle: ";
-			cin>>r;
-			if(r<0)
-				r=-r;
-		}
-		float circleArea()
-		{
-			return 3.14*r*r;
-		}
-};
-class Area{
-	private:
-		int r,len,bre,side;
-	public:
-		void getDataCircle(int a)
-		{
-			r=a;
-		}
-		void getDataRectangle(int a,int b)
-		{
-			len=a;
-			bre=b;
-		}
-		void getDataSquare(int a)
-		{
-			side=a;
-		}
-		void circleArea()
-		{
-			cout<<endl<<"Area of circle is: "<<r*r*3.14;
-		}
-		void rectangleArea()
-		{
-			cout<<endl<<"Area of rectangle is: "<<len*bre;
-		}
-		void squareArea()
-		{
-			cout<<endl<<"Area of square is: "<<side*side;
-		}
-};
 int main()
 {
 	cout<<"\n\n\e[1mQuestion1. Define a class Complex to represent a complex number. Declare instance member variables to store real and imaginary part of a complex number, also define instance member functions to set values of complex number and print values of complex number.\e[m";
-	Complex c1,c2;
+	assign25::Complex c1,c2;
 	c1.getValue();
 	c1.disPlay();
 	c2.getValue();
 	c2.disPlay();
 
 	cout<<"\n\n\e[1mQuestion2. Define a class Time to represent Time (like 3 hr 45 min 20 sec). Declare appropriate number of instance member variables and also define instance member functions to set values for time and display values of time..\e[m";
-	Time t1;
+	assign25::Time t1;
 	t1.getValue();
 	t1.disPlay();
 
 	cout<<"\n\n\e[1mQuestion3. Define a class Factorial and define an instance member function to find the Factorial of a number using class..\e[m";
-	Factorial f1;
+	assign25::Factorial f1;
 	f1.getVal();
 	cout<<"The factorial of number is "<<f1.fact();
 
 	cout<<"\n\n\e[1mQuestion4. Define a class LargestNumber and define an instance member function to find the Largest of three Numbers using the class..\e[m";
-	LargestNumber l1;
+	assign25::LargestNumber l1;
 	l1.getVal();
 	cout<<"The largest of three number is "<<l1.largestNum();
 
 	cout<<"\n\n\e[1mQuestion5. Define a class ReverseNumber and define an instance member function to find Reverse of a Number using class..\e[m";
-	ReverseNumber r1;
+	assign25::ReverseNumber r1;
 	r1.getVal();
 	r1.numRevMain();
 
 	printf("\n\n\e[1mQuestion6. Define a class Square to find the square of a number and write a C++ program to Count number of times a function is called..\e[m");
-	Square s1;
+	assign25::Square s1;
 	s1.getVal();
 	cout<<"The square of given number is "<<s1.numSq();
 	
 	printf("\n\n\e[1mQuestion7. Define a class Greatest and define instance member function to find Largest among 3 numbers using classes..\e[m");
-	Greatest g1;
+	assign25::Greatest g1;
 	g1.getVal();
 	cout<<"The largest of three number is "<<g1.greatestNum();
 
 	printf("\n\n\e[1mQuestion8. Define a class Rectangle and define an instance member function to find the area of the rectangle..\e[m");
-	Rectangle rec1;
+	assign25::Rectangle rec1;
 	rec1.getVal();
 	cout<<"Area of rectangle is "<<rec1.recArea();
 
 	printf("\n\n\e[1mQuestion9. Define a class Circle and define an instance member function to find the area of the circle..\e[m");
-	Circle cir1;
+	assign25::Circle cir1;
 	cir1.getVal();
 	cout<<"Area of the circle is "<<cir1.circleArea();
 
 	printf("\n\n\e[1mQuestion10. Define a class Area and define instance member functions to find the area of the different shapes like square, rectangle , circle etc..\e[m");
-	Area a1;
+	assign25::Area a1;
 	a1.getDataCircle(5);
 	a1.circleArea();
 	a1.getDataRectangle(6,4);
--- ticTacToeGame.cpp
+++ ticTacToeGame.cpp
@@ -1,7 +1,481 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
+void smartComputer(char[]);
+void evilComputer(char[]);
+void display(char[]);
+int match(char[]);
+void compInput(char[],int,int[]);
+void evilCompInput(char x[],int a,int b[]);
+void player(char[]);
+// namespace ticTacToe
+// {
+
+// };
+static int tC=0;
 int main()
 {
+    int input;char a[10]={'0','1','2','3','4','5','6','7','8','9'},b;
+    cout<<"Enter your choice"<<endl<<"1. Want to play with Smart Computer"<<endl<<"2. Want to play with Evil Computer"<<endl<<"3. Want to play with player"<<endl<<"4. Exit"<<endl;
+    cin>>input;
+    system("clear");
+
+    switch(input)
+    {
+        case 1:
+            smartComputer(a);
+            break;
+
+        case 2:
+            evilComputer(a);
+            break;
+
+        case 3:
+            player(a);
+            break;
+
+        default:
+            break;
+    }
 
     return 0;
+}
+//functions
+void player(char x[])
+{
+    int flag=0,a,b[10]={0};
+    do
+    {
+        if(tC<9)
+        {
+            for(int i=1;i<=2;i++)
+            {
+                display(x);
+                cout<<"Player -'"<<i<<"' turn, please enter position: ";
+                cin>>a;
+                if(b[a]==0 && a>=1 && a<=9)
+                {
+                    b[a]=1;
+                    tC++;
+                    if(i==1)
+                        x[a]='x';
+                    else
+                        x[a]='o';
+
+                    flag = match(x);
+
+                    if(flag)
+                    {
+                        system("clear");
+                        display(x);
+                        cout<<"Player '"<<i<<"' - You won !!"<<endl<<endl;
+                        cout<<"###########################################################################"<<endl<<endl;
+                        break;
+                    }
+                }
+                else
+                {
+                    system("clear");
+                    cout<<"\033[0;31mInvalid position!!\033[0m"<<endl<<endl;
+                }
+                system("clear");
+            }
+        }
+        else
+        {
+            system("clear");
+            display(x);
+            cout<<"Game Draw !!"<<endl<<endl;
+            cout<<"###########################################################################"<<endl<<endl;
+            break;
+        }
+    }
+    while(!flag);
+}
+void evilComputer(char x[])
+{
+    int flag=0,a,b[10]={0};
+    do
+    {
+        display(x);
+        cout<<"Your turn, please enter position: ";
+        cin>>a;
+        if(b[a]==0 && a>=1 && a<=9)
+        {
+            b[a]=1;
+            tC++;
+            x[a]='x';
+
+            flag = match(x);
+
+            if(flag)
+            {
+                system("clear");
+                display(x);
+                cout<<"You won !!"<<endl<<endl;
+                cout<<"###########################################################################"<<endl<<endl;
+                break;
+            }
+
+            evilCompInput(x,a,b);
+
+            flag = match(x);
+
+            if(flag)
+            {
+                system("clear");
+                display(x);
+                cout<<"You lose !!"<<endl<<endl;
+                cout<<"###########################################################################"<<endl<<endl;
+                break;
+            }
+
+
+            if(tC==9)
+            {
+                system("clear");
+                display(x);
+                cout<<"Game Draw !!"<<endl<<endl;
+                cout<<"###########################################################################"<<endl<<endl;
+                break;
+            }
+
+            system("clear");
+        }
+        else
+        {
+            system("clear");
+            cout<<"\033[0;31mInvalid position!!\033[0m"<<endl<<endl;
+        }
+    }
+    while(!flag);
+}
+void smartComputer(char x[])
+{
+    int flag=0,a,b[10]={0};
+    do
+    {
+        display(x);
+        cout<<"Your turn, please enter position: ";
+        cin>>a;
+        if(b[a]==0 && a>=1 && a<=9)
+        {
+            b[a]=1;
+            tC++;
+            x[a]='x';
+
+            flag = match(x);
+
+            if(flag)
+            {
+                system("clear");
+                display(x);
+                cout<<"You won !!"<<endl<<endl;
+                cout<<"###########################################################################"<<endl<<endl;
+                break;
+            }
+
+            compInput(x,a,b);
+
+            flag = match(x);
+
+            if(flag)
+            {
+                system("clear");
+                display(x);
+                cout<<"You lose !!"<<endl<<endl;
+                cout<<"###########################################################################"<<endl<<endl;
+                break;
+            }
+
+
+            if(tC==9)
+            {
+                system("clear");
+                display(x);
+                cout<<"Game Draw !!"<<endl<<endl;
+                cout<<"###########################################################################"<<endl<<endl;
+                break;
+            }
+
+            system("clear");
+        }
+        else
+        {
+            system("clear");
+            cout<<"\033[0;31mInvalid position!!\033[0m"<<endl<<endl;
+        }
+    }
+    while(!flag);
+}
+void evilCompInput(char x[],int a,int b[])
+{
+    if(b[7]!=1 && x[1]=='o' && x[4]=='o')
+        {x[7]='o';b[7]=1;tC++;}
+    if(b[8]!=1 && x[2]=='o' && x[5]=='o')
+        {x[8]='o';b[8]=1;tC++;}
+    if(b[9]!=1 && x[3]=='o' && x[6]=='o')
+        {x[9]='o';b[9]=1;tC++;}
+    if(b[1]!=1 && x[4]=='o' && x[7]=='o')
+        {x[1]='o';b[1]=1;tC++;}
+    if(b[2]!=1 && x[5]=='o' && x[8]=='o')
+        {x[2]='o';b[2]=1;tC++;}
+    if(b[3]!=1 && x[6]=='o' && x[9]=='o')
+        {x[3]='o';b[3]=1;tC++;}
+
+    if(b[3]!=1 && x[1]=='o' && x[2]=='o')
+        {x[3]='o';b[3]=1;tC++;}
+    if(b[6]!=1 && x[4]=='o' && x[5]=='o')
+        {x[6]='o';b[6]=1;tC++;}
+    if(b[9]!=1 && x[7]=='o' && x[8]=='o')
+        {x[9]='o';b[9]=1;tC++;}
+    if(b[1]!=1 && x[2]=='o' && x[3]=='o')
+        {x[1]='o';b[1]=1;tC++;}
+    if(b[4]!=1 && x[5]=='o' && x[6]=='o')
+        {x[4]='o';b[4]=1;tC++;}
+    if(b[7]!=1 && x[8]=='o' && x[9]=='o')
+        {x[7]='o';b[7]=1;tC++;}
+
+    if(b[9]!=1 && x[1]=='o' && x[5]=='o')
+        {x[9]='o';b[5]=1;tC++;}
+    if(b[1]!=1 && x[5]=='o' && x[9]=='o')
+        {x[1]='o';b[1]=1;tC++;}
+    if(b[3]!=1 && x[7]=='o' && x[5]=='o')
+        {x[3]='o';b[3]=1;tC++;}
+    if(b[7]!=1 && x[5]=='o' && x[3]=='o')
+        {x[7]='o';b[7]=1;tC++;}
+
+    if(b[2]!=1 && x[1]=='o' && x[3]=='o')
+        {x[2]='o';b[2]=1;tC++;}
+    if(b[5]!=1 && x[4]=='o' && x[6]=='o')
+        {x[5]='o';b[5]=1;tC++;}
+    if(b[8]!=1 && x[7]=='o' && x[9]=='o')
+        {x[8]='o';b[8]=1;tC++;}
+    if(b[4]!=1 && x[1]=='o' && x[7]=='o')
+        {x[4]='o';b[4]=1;tC++;}
+    if(b[5]!=1 && x[2]=='o' && x[8]=='o')
+        {x[5]='o';b[5]=1;tC++;}
+    if(b[6]!=1 && x[3]=='o' && x[9]=='o')
+        {x[6]='o';b[6]=1;tC++;}
+    if(b[5]!=1 && x[1]=='o' && x[9]=='o')
+        {x[5]='o';b[5]=1;tC++;}
+    if(b[5]!=1 && x[3]=='o' && x[7]=='o')
+        {x[5]='o';b[5]=1;tC++;}
+
+    if(b[7]!=1 && x[1]=='x' && x[4]=='x')
+        {x[7]='o';b[7]=1;tC++;}
+    if(b[8]!=1 && x[2]=='x' && x[5]=='x')
+        {x[8]='o';b[8]=1;tC++;}
+    if(b[9]!=1 && x[3]=='x' && x[6]=='x')
+        {x[9]='o';b[9]=1;tC++;}
+    if(b[1]!=1 && x[4]=='x' && x[7]=='x')
+        {x[1]='o';b[1]=1;tC++;}
+    if(b[2]!=1 && x[5]=='x' && x[8]=='x')
+        {x[2]='o';b[2]=1;tC++;}
+    if(b[3]!=1 && x[6]=='x' && x[9]=='x')
+        {x[3]='o';b[3]=1;tC++;}
+
+    if(b[9]!=1 && x[1]=='x' && x[5]=='x')
+        {x[9]='o';b[5]=1;tC++;}
+    if(b[1]!=1 && x[5]=='x' && x[9]=='x')
+        {x[1]='o';b[1]=1;tC++;}
+    if(b[3]!=1 && x[7]=='x' && x[5]=='x')
+        {x[3]='o';b[3]=1;tC++;}
+    if(b[7]!=1 && x[5]=='x' && x[3]=='x')
+        {x[7]='o';b[7]=1;tC++;}
+
+    if(b[3]!=1 && x[1]=='x' && x[2]=='x')
+        {x[3]='o';b[3]=1;tC++;}
+    if(b[6]!=1 && x[4]=='x' && x[5]=='x')
+        {x[6]='o';b[6]=1;tC++;}
+    if(b[9]!=1 && x[7]=='x' && x[8]=='x')
+        {x[9]='o';b[9]=1;tC++;}
+    if(b[1]!=1 && x[2]=='x' && x[3]=='x')
+        {x[1]='o';b[1]=1;tC++;}
+    if(b[4]!=1 && x[5]=='x' && x[6]=='x')
+        {x[4]='o';b[4]=1;tC++;}
+    if(b[7]!=1 && x[8]=='x' && x[9]=='x')
+        {x[7]='o';b[7]=1;tC++;}
+
+    if(b[2]!=1 && x[1]=='x' && x[3]=='x')
+        {x[2]='o';b[2]=1;tC++;}
+    if(b[5]!=1 && x[4]=='x' && x[6]=='x')
+        {x[5]='o';b[5]=1;tC++;}
+    if(b[8]!=1 && x[7]=='x' && x[9]=='x')
+        {x[8]='o';b[8]=1;tC++;}
+    if(b[4]!=1 && x[1]=='x' && x[7]=='x')
+        {x[4]='o';b[4]=1;tC++;}
+    if(b[5]!=1 && x[2]=='x' && x[8]=='x')
+        {x[5]='o';b[5]=1;tC++;}
+    if(b[6]!=1 && x[3]=='x' && x[9]=='x')
+        {x[6]='o';b[6]=1;tC++;}
+    if(b[5]!=1 && x[1]=='x' && x[9]=='x')
+        {x[5]='o';b[5]=1;tC++;}
+    if(b[5]!=1 && x[3]=='x' && x[7]=='x')
+        {x[5]='o';b[5]=1;tC++;}
+    if(tC<9)
+    {
+        for(int i=1;i<=9;i++)
+        {
+            if(b[i]==0)
+            {
+                x[i]='o';
+                b[i]=1;
+                tC++;
+                break;
+            }
+        }
+    }
+}
+void compInput(char x[],int a,int b[])
+{
+    if(b[7]!=1 && x[1]=='o' && x[4]=='o')
+        {x[7]='o';b[7]=1;tC++;}
+    else if(b[8]!=1 && x[2]=='o' && x[5]=='o')
+        {x[8]='o';b[8]=1;tC++;}
+    else if(b[9]!=1 && x[3]=='o' && x[6]=='o')
+        {x[9]='o';b[9]=1;tC++;}
+    else if(b[1]!=1 && x[4]=='o' && x[7]=='o')
+        {x[1]='o';b[1]=1;tC++;}
+    else if(b[2]!=1 && x[5]=='o' && x[8]=='o')
+        {x[2]='o';b[2]=1;tC++;}
+    else if(b[3]!=1 && x[6]=='o' && x[9]=='o')
+        {x[3]='o';b[3]=1;tC++;}
+
+    else if(b[3]!=1 && x[1]=='o' && x[2]=='o')
+        {x[3]='o';b[3]=1;tC++;}
+    else if(b[6]!=1 && x[4]=='o' && x[5]=='o')
+        {x[6]='o';b[6]=1;tC++;}
+    else if(b[9]!=1 && x[7]=='o' && x[8]=='o')
+        {x[9]='o';b[9]=1;tC++;}
+    else if(b[1]!=1 && x[2]=='o' && x[3]=='o')
+        {x[1]='o';b[1]=1;tC++;}
+    else if(b[4]!=1 && x[5]=='o' && x[6]=='o')
+        {x[4]='o';b[4]=1;tC++;}
+    else if(b[7]!=1 && x[8]=='o' && x[9]=='o')
+        {x[7]='o';b[7]=1;tC++;}
+
+    else if(b[2]!=1 && x[1]=='o' && x[3]=='o')
+        {x[2]='o';b[2]=1;tC++;}
+    else if(b[5]!=1 && x[4]=='o' && x[6]=='o')
+        {x[5]='o';b[5]=1;tC++;}
+    else if(b[8]!=1 && x[7]=='o' && x[9]=='o')
+        {x[8]='o';b[8]=1;tC++;}
+    else if(b[4]!=1 && x[1]=='o' && x[7]=='o')
+        {x[4]='o';b[4]=1;tC++;}
+    else if(b[5]!=1 && x[2]=='o' && x[8]=='o')
+        {x[5]='o';b[5]=1;tC++;}
+    else if(b[6]!=1 && x[3]=='o' && x[9]=='o')
+        {x[6]='o';b[6]=1;tC++;}
+    else if(b[5]!=1 && x[1]=='o' && x[9]=='o')
+        {x[5]='o';b[5]=1;tC++;}
+    else if(b[5]!=1 && x[3]=='o' && x[7]=='o')
+        {x[5]='o';b[5]=1;tC++;}
+
+    else if(b[9]!=1 && x[1]=='o' && x[5]=='o')
+        {x[9]='o';b[5]=1;tC++;}
+    else if(b[1]!=1 && x[5]=='o' && x[9]=='o')
+        {x[1]='o';b[1]=1;tC++;}
+    else if(b[3]!=1 && x[7]=='o' && x[5]=='o')
+        {x[3]='o';b[3]=1;tC++;}
+    else if(b[7]!=1 && x[5]=='o' && x[3]=='o')
+        {x[7]='o';b[7]=1;tC++;}
+
+    else if(b[7]!=1 && x[1]=='x' && x[4]=='x')
+        {x[7]='o';b[7]=1;tC++;}
+    else if(b[8]!=1 && x[2]=='x' && x[5]=='x')
+        {x[8]='o';b[8]=1;tC++;}
+    else if(b[9]!=1 && x[3]=='x' && x[6]=='x')
+        {x[9]='o';b[9]=1;tC++;}
+    else if(b[1]!=1 && x[4]=='x' && x[7]=='x')
+        {x[1]='o';b[1]=1;tC++;}
+    else if(b[2]!=1 && x[5]=='x' && x[8]=='x')
+        {x[2]='o';b[2]=1;tC++;}
+    else if(b[3]!=1 && x[6]=='x' && x[9]=='x')
+        {x[3]='o';b[3]=1;tC++;}
+
+    else if(b[3]!=1 && x[1]=='x' && x[2]=='x')
+        {x[3]='o';b[3]=1;tC++;}
+    else if(b[6]!=1 && x[4]=='x' && x[5]=='x')
+        {x[6]='o';b[6]=1;tC++;}
+    else if(b[9]!=1 && x[7]=='x' && x[8]=='x')
+        {x[9]='o';b[9]=1;tC++;}
+    else if(b[1]!=1 && x[2]=='x' && x[3]=='x')
+        {x[1]='o';b[1]=1;tC++;}
+    else if(b[4]!=1 && x[5]=='x' && x[6]=='x')
+        {x[4]='o';b[4]=1;tC++;}
+    else if(b[7]!=1 && x[8]=='x' && x[9]=='x')
+        {x[7]='o';b[7]=1;tC++;}
+
+    else if(b[9]!=1 && x[1]=='x' && x[5]=='x')
+        {x[9]='o';b[5]=1;tC++;}
+    else if(b[1]!=1 && x[5]=='x' && x[9]=='x')
+        {x[1]='o';b[1]=1;tC++;}
+    else if(b[3]!=1 && x[7]=='x' && x[5]=='x')
+        {x[3]='o';b[3]=1;tC++;}
+    else if(b[7]!=1 && x[5]=='x' && x[3]=='x')
+        {x[7]='o';b[7]=1;tC++;}
+
+    else if(b[2]!=1 && x[1]=='x' && x[3]=='x')
+        {x[2]='o';b[2]=1;tC++;}
+    else if(b[5]!=1 && x[4]=='x' && x[6]=='x')
+        {x[5]='o';b[5]=1;tC++;}
+    else if(b[8]!=1 && x[7]=='x' && x[9]=='x')
+        {x[8]='o';b[8]=1;tC++;}
+    else if(b[4]!=1 && x[1]=='x' && x[7]=='x')
+        {x[4]='o';b[4]=1;tC++;}
+    else if(b[5]!=1 && x[2]=='x' && x[8]=='x')
+        {x[5]='o';b[5]=1;tC++;}
+    else if(b[6]!=1 && x[3]=='x' && x[9]=='x')
+        {x[6]='o';b[6]=1;tC++;}
+    else if(b[5]!=1 && x[1]=='x' && x[9]=='x')
+        {x[5]='o';b[5]=1;tC++;}
+    else if(b[5]!=1 && x[3]=='x' && x[7]=='x')
+        {x[5]='o';b[5]=1;tC++;}
+    else
+    {
+        for(int i=1;i<=9;i++)
+        {
+            if(b[i]==0)
+            {
+                x[i]='o';
+                b[i]=1;
+                tC++;
+                break;
+            }
+        }
+    }
+}
+void display(char x[])
+{
+    cout<<endl<<"############################## Tic Tac Toe ##############################"<<endl<<endl;
+    cout<<"First Player x"<<endl<<"Second Player o"<<endl<<endl;
+    cout<<"   |   |   "<<endl;
+    cout<<" "<<x[1]<<" | "<<x[2]<<" | "<<x[3]<<endl;
+    cout<<"___|___|___"<<endl;
+    cout<<"   |   |   "<<endl;
+    cout<<" "<<x[4]<<" | "<<x[5]<<" | "<<x[6]<<endl;
+    cout<<"___|___|___"<<endl;
+    cout<<"   |   |   "<<endl;
+    cout<<" "<<x[7]<<" | "<<x[8]<<" | "<<x[9]<<endl;
+    cout<<"___|___|___"<<endl;
+    cout<<endl<<"#########################################################################"<<endl<<endl;
+}
+int match(char x[])
+{
+    if((x[1]==x[2] && x[2]==x[3]) ||
+        (x[4]==x[5] && x[5]==x[6]) ||
+        (x[7]==x[8] && x[8]==x[9]) ||
+        (x[1]==x[4] && x[4]==x[7]) ||
+        (x[2]==x[5] && x[5]==x[8]) ||
+        (x[3]==x[6] && x[6]==x[9]) ||
+        (x[1]==x[5] && x[5]==x[9]) ||
+        (x[3]==x[5] && x[5]==x[7]))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
