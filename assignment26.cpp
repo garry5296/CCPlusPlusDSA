9eb030737c053b353f089809a2a4d909e97d007b
garry5296 <garry5296@gmail.com>
2025-01-25 23:43:47 +0530

    assignment26.cpp

--- assignment26.cpp
+++ assignment26.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int main()
 {
+    cout<<endl<<endl<<"\e[1mQuestion1. Define a class Complex to represent a complex number with instance variables a \
+and"<<endl<<"b to store real and imaginary parts. Also define following member functions"<<endl<<"a. void setData(int,int)" \
+<<endl<<"b. void showData()"<<endl<<"c. Complex add(Complex).\e[m";
 
     return 0;
 }
