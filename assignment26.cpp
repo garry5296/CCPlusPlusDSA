f627bad825d418204e8fa690a562669d52f08719
garry5296 <garry5296@gmail.com>
2025-01-25 23:51:42 +0530

    assignment26.cpp

--- assignment26.cpp
+++ assignment26.cpp
@@ -6,5 +6,19 @@ int main()
 and"<<endl<<"b to store real and imaginary parts. Also define following member functions"<<endl<<"a. void setData(int,int)" \
 <<endl<<"b. void showData()"<<endl<<"c. Complex add(Complex).\e[m";
 
+    cout<<endl<<endl<<"\e[1mQuestion2. Define a class Time to represent a time with instance variables h,m and s to store \
+\nhour, minute and second. Also define following member functions \
+\na. void setTime(int,int,int) \
+\nb. void showTime() \
+\nc. void normalize() \
+\nd. Time add(Time)\e[m";
+
+    cout<<endl<<endl<<"\e[1mQuestion3. Define a class Cube and calculate Volume of Cube and initialise it using constructor..\e[m";
+    cout<<endl<<endl<<"\e[1mQuestion4. Define a class Counter and Write a program to Show Counter using Constructor..\e[m";
+    cout<<endl<<endl<<"\e[1mQuestion5. Define a class Date and write a program to Display Dateand initialise date object using Constructors..\e[m";
+    cout<<endl<<endl<<"\e[1mQuestion6. Define a class student and write a program to enter student details using constructor and define member function to display all the details..\e[m";
+    cout<<endl<<endl<<"\e[1mQuestion7. Define a class Box and write a program to enter length, breadth and height and initialise objects using constructor also define member functions to calculate volume of the box..\e[m";
+    cout<<endl<<endl<<"\e[1mQuestion8. Define a class Bank and define member functions to read principal , rate of interest and year. Another member functions to calculate simple interest and display it.Initialise all details using constructor..\e[m";
+
     return 0;
 }
